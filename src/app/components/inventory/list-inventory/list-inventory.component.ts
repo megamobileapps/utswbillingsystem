8d0f48e61f5d1c037b9a394214781cc903244d7b
Pankaj goel <goel74@yahoo.com>
2023-12-26 06:10:38 +0530

    support to view inventory with sort and pagination

--- src/app/components/inventory/list-inventory/list-inventory.component.ts
+++ src/app/components/inventory/list-inventory/list-inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, OnChanges, SimpleChanges} from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -6,34 +6,19 @@ import { DataService } from 'src/app/services/data.service';
 import { InventoryService } from 'src/app/services/inventory.service';
 import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
 
 @Component({
   selector: 'app-list-inventory',
   templateUrl: './list-inventory.component.html',
   styleUrls: ['./list-inventory.component.css']
 })
-export class ListInventoryComponent implements OnInit,AfterViewInit {
+export class ListInventoryComponent implements OnInit,AfterViewInit,OnChanges  {
 
   
   @Input() barcode:string|null=null
+  @Input() filterwithbarcode:string|null=null
+
   @ViewChild(MatPaginator) private paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
@@ -76,7 +61,7 @@ export class ListInventoryComponent implements OnInit,AfterViewInit {
     this.getAllInventory();
   }
 
-  getAllInventory(){
+  getAllInventory(filterwith=''){
     let self = this;
      this._inventoryService.getAllInventory().subscribe(data => { 
        console.log('getAllInventory(): step 1', JSON.stringify(data));
@@ -85,13 +70,24 @@ export class ListInventoryComponent implements OnInit,AfterViewInit {
        for(let i=0;i<data.length;i++){
          console.log('getAllInventory(): step 2', JSON.stringify(data[i]));
          let datekeys = Object.keys(data[i])
-         rcvddata.push( data[i][datekeys[0]].itemdetails );        
+         console.log('getAllInventory(): filter value is '+this.filterwithbarcode);
+         if (this.filterwithbarcode != null || filterwith != ''){
+          if(data[i][datekeys[0]].itemdetails['barcode'] == this.filterwithbarcode??filterwith)
+            rcvddata.push( data[i][datekeys[0]].itemdetails ); 
+         }else{
+          rcvddata.push( data[i][datekeys[0]].itemdetails );        
+         }
          }
          this.dataSource.data = rcvddata;
          console.log('getAllInventory(): Data after filling ', JSON.stringify(rcvddata));
        
      });
    }
+   ngOnChanges(changes: SimpleChanges) {
+    // changes.prop contains the old and the new value...
+    console.log('ListInventoryComponent:ngOnChanges()'+JSON.stringify(changes));
+    this.getAllInventory(changes["filterwithbarcode"]["currentValue"]);
+  }
    /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
     // This example uses English messages. If your application supports
